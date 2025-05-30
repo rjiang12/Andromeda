eec15c22d3d5b70caf3c61e18c1cc3fc9fb5d737
rjiang12 <roy.jiang.4757@gmail.com>
2025-05-30 10:25:11 -0700

    name workflow added

--- clock/clock.js
+++ clock/clock.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const clockElement = document.getElementById("clock");
+    const greetingElement = document.getElementById("greeting");
+
     const updateClock = () => {
         const now = new Date();
         const hours = String(now.getHours()).padStart(2, '0');
@@ -7,21 +9,49 @@ document.addEventListener("DOMContentLoaded", () => {
         const ampm = hours >= 12 ? 'PM' : 'AM';
         clockElement.textContent = `${hours}:${minutes} ${ampm}`;
     };
-    updateClock();
-    setInterval(updateClock, 1000);
 
-    const greetingElement = document.getElementById("greeting");
     const updateGreeting = () => {
+        const name = localStorage.getItem("name");
         const now = new Date();
         const hours = now.getHours();
         let greeting = "Good morning";
+    
         if (hours >= 12 && hours < 18) {
             greeting = "Good afternoon";
         } else if (hours >= 18) {
             greeting = "Good evening";
         }
-        greetingElement.textContent = greeting;
-    }
+    
+        if (!name) {
+            let nameInput = document.querySelector(".name-input");
+            if (!nameInput) {
+                nameInput = document.createElement("input");
+                nameInput.type = "text";
+                nameInput.placeholder = "What is your name?";
+                nameInput.id = "name-input";
+    
+                nameInput.addEventListener("keydown", (event) => {
+                    if (event.key === "Enter") {
+                        const enteredName = nameInput.value.trim();
+                        if (enteredName) {
+                            localStorage.setItem("name", enteredName.trim());
+                            updateGreeting(); 
+                        }
+                    }
+                });
+    
+                greetingElement.innerHTML = "";
+                greetingElement.appendChild(nameInput);
+                nameInput.focus(); 
+            }
+        } else {
+            greetingElement.textContent = `${greeting}, ${name}`;
+        }
+    };
+
+    updateClock();
+    setInterval(updateClock, 1000);
+
     updateGreeting();
-    setInterval(updateGreeting, 1000 * 3600);
+    setInterval(updateGreeting, 1000 * 3600); 
 });
